03968d22cf494c57697a75eb2372c79aff802ac3
Rui Constancio <r.miguel.a.c@gmail.com>
2026-02-22 19:19:14 -0300

    Fix grouping - move diversity to player

--- js/animation.js
+++ js/animation.js
@@ -131,11 +131,11 @@ export function initAnimation({ chart, state, raw, months, processData }) {
   d3.select("#btn-winrate").on("click", () => switchView("winrate"));
   d3.select("#btn-attendance").on("click", () => switchView("attendance"));
   d3.select("#btn-playerdrawrate").on("click", () => switchView("playerdrawrate"));
+  d3.select("#btn-deckdiv").on("click", () => switchView("deckdiv"));
 
   // Deck sub-views
   d3.select("#btn-deckwins").on("click", () => switchView("deckwins"));
   d3.select("#btn-deckpop").on("click", () => switchView("deckpop"));
-  d3.select("#btn-deckdiv").on("click", () => switchView("deckdiv"));
   d3.select("#btn-deckdrawrate").on("click", () => switchView("deckdrawrate"));
   d3.select("#btn-deckwinrate").on("click", () => switchView("deckwinrate"));
 
